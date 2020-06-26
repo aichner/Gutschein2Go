2f85730ef2cb006e65e6d65ff4e45a71f267fafe
Christian Aichner <contact@aichner-christian.com>
2020-06-26 10:19:27 +0200

    Improve Select Page
    Hotels/Restaurant section has been added. The map selection is now shown as an active item.

--- src/components/pages/SelectPage/index.jsx
+++ src/components/pages/SelectPage/index.jsx
@@ -39,12 +39,63 @@ class SelectPage extends React.Component {
 
   render() {
     return (
-      <MDBContainer id="selector">
-        <div className="mapcontainer">
-          <RegionPicker setRegion={this.setRegion} />
-        </div>
-        <div className="companytypeselector"></div>
-      </MDBContainer>
+      <div id="selector">
+        <MDBContainer className="text-center">
+          <h2 className="font-weight-bold">Welcher Gutschein darf es sein?</h2>
+          <hr />
+          <h3 className="text-center">Sie wissen wo es hingehen soll?</h3>
+          <p className="text-muted">
+            Wählen Sie das gewünschte Bundesland aus, um alle Hotels,
+            Restaurants und Betriebe in diesem anzuzeigen.
+          </p>
+          <div className="mapcontainer">
+            <RegionPicker
+              setRegion={this.setRegion}
+              selected={this.state.region}
+            />
+          </div>
+          <hr />
+        </MDBContainer>
+        <MDBContainer className="text-center" fluid>
+          <h3 className="text-center">Sie wissen was Sie wollen?</h3>
+          <p className="text-muted">Wählen Sie die gewünschte Kategorie aus.</p>
+          <div className="split-selector">
+            <MDBRow>
+              <MDBCol lg="6">
+                <MDBView>
+                  <MDBMask className="flex-center" overlay="blue-light">
+                    <div>
+                      <h2 className="font-weight-bold mb-2">Hotels</h2>
+                      <div className="stars amber-text mb-2">
+                        <MDBIcon icon="star" size="sm" />
+                        <MDBIcon icon="star" size="sm" />
+                        <MDBIcon icon="star" size="sm" />
+                        <MDBIcon icon="star" size="sm" />
+                        <MDBIcon icon="star" size="sm" />
+                      </div>
+                      <p className="lead">
+                        Ausgewählte Hotels in ganz Österreich
+                      </p>
+                    </div>
+                  </MDBMask>
+                </MDBView>
+              </MDBCol>
+              <MDBCol lg="6">
+                <MDBView>
+                  <MDBMask className="flex-center" overlay="pink-light">
+                    <div>
+                      <h2 className="font-weight-bold mb-2">
+                        Restaurants & Mehr
+                      </h2>
+                      <p className="lead">Jetzt Vielfalt entdecken</p>
+                    </div>
+                  </MDBMask>
+                </MDBView>
+              </MDBCol>
+            </MDBRow>
+          </div>
+        </MDBContainer>
+      </div>
     );
   }
 }
